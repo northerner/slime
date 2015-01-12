8fa7c4b066729e69c02c7299d07512f6ad270849
Daniel King <dan@llamadigital.net>
2015-01-12 00:27:30 +0000

    add variable length html tags with text content

--- lib/slime.ex
+++ lib/slime.ex
@@ -6,7 +6,12 @@ defmodule Slime do
   def parse(<<"| " :: binary, text :: binary>>), do: text
   def parse(<<"= " :: binary, expression :: binary>>), do: to_eex(expression) 
   def parse(<<"<", html_string :: binary>>), do: "<" <> html_string
-  def parse(tag), do: "<#{tag}></#{tag}>" 
+  def parse(line) do
+    case String.split(line) do
+      [tag | content] -> "<#{tag}>#{Enum.join(content, " ")}</#{tag}>" 
+      tag -> "<#{tag}></#{tag}>" 
+    end
+  end
 
   defp to_eex(expression), do: "<%= #{expression} %>"
 end
--- test/slime_test.exs
+++ test/slime_test.exs
@@ -13,7 +13,12 @@ defmodule SlimeTest do
     assert Slime.to_html("= 1 + 1") == "2"
   end
 
-  test "html tags" do
+  test "empty html tags" do
     assert Slime.to_html("p") == "<p></p>"
+  end
+
+  test "html tags with content" do
+    assert Slime.to_html("p this is a test paragraph") == "<p>this is a test paragraph</p>"
+    assert Slime.to_html("div this is a test div") == "<div>this is a test div</div>"
   end
 end
